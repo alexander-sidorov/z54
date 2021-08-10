6e9a0b87043d24207933a040b6b1d5b5163f9744
Alexander Sidorov <alexander@sidorov.dev>
2021-08-10 20:52:13 +0300

    start

---
+++ asgi.py
@@ -0,0 +1,34 @@
+import json
+import lessons
+
+
+async def application(scope, receive, send) -> None:
+    if scope["type"] != "http":
+        return
+
+    path = scope["path"]
+
+    await send(
+        {
+            "headers": [
+                [b"content-type", b"application/json"],
+            ],
+            "status": 200,
+            "type": "http.response.start",
+        }
+    )
+
+    payload = {
+        "hello": "world",
+        "path": path,
+    }
+
+    body = json.dumps(payload).encode()
+
+    await send(
+        {
+            "body": body,
+            "type": "http.response.body",
+        }
+    )
+
