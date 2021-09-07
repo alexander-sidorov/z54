5251c30f87c43c828891ab2b0704266acc589e83
Alexander Sidorov <alexander@sidorov.dev>
2021-09-07 21:11:38 +0300

    fixed settings

--- config.py
+++ config.py
@@ -1,25 +1,19 @@
-import os
-
-admin_token = os.getenv("ADMIN_TOKEN")
-print(f"{admin_token = !r}")
-
-webhook_secret = os.getenv("WEBHOOK_SECRET")
-assert webhook_secret, "env var WEBHOOK_SECRET is not set"
-webhook_path = f"/tg/wh{webhook_secret}"
-print(f"{webhook_path = !r}")
-
-bot_token = os.getenv("TG_BOT_TOKEN")
-assert bot_token, "env var TG_BOT_TOKEN is not set"
-bot_url = f"https://api.telegram.org/bot{bot_token}"
-print(f"{bot_url = !r}")
+from pydantic import BaseSettings
+from pydantic import Field
 
 
-class Settings:
-    admin_token = admin_token
-    bot_token = bot_token
-    bot_url = bot_url
-    webhook_path = webhook_path
-    webhook_secret = webhook_secret
+class Settings(BaseSettings):
+    admin_token: str = Field(..., env="ADMIN_TOKEN")
+    bot_token: str = Field(..., env="TG_BOT_TOKEN")
+    webhook_secret: str = Field(..., env="WEBHOOK_SECRET")
+
+    @property
+    def bot_url(self):
+        return f"https://api.telegram.org/bot{self.bot_token}"
+
+    @property
+    def webhook_path(self):
+        return f"/tg/wh{self.webhook_secret}"
 
 
 settings = Settings()
