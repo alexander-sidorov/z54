3f1a3b82a1cfa0465c217d9b215409cada3599ae
Alexander Sidorov <alexander@sidorov.dev>
2021-09-21 21:29:59 +0300

    added links to all posts/single post

--- superproject/blog/models.py
+++ superproject/blog/models.py
@@ -5,3 +5,6 @@ class Post(models.Model):
     title = models.TextField()
     content = models.TextField(blank=True, null=True)
     hidden = models.BooleanField(default=False)
+
+    def get_absolute_url(self) -> str:
+        return f"/blog/{self.pk}/"
