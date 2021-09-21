cfc4c9a55ba6e3d608746c7210b34bb786491f4a
Alexander Sidorov <alexander@sidorov.dev>
2021-09-21 21:17:47 +0300

    added view for single post

--- superproject/blog/urls.py
+++ superproject/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from blog.views import AllPostsView
+from blog.views import SinglePostView
 
 urlpatterns = [
     path("", AllPostsView.as_view()),
+    path("<int:pk>/", SinglePostView.as_view()),
 ]
--- superproject/blog/views.py
+++ superproject/blog/views.py
@@ -1,7 +1,12 @@
+from django.views.generic import DetailView
 from django.views.generic import ListView
 
 from blog.models import Post
 
 
 class AllPostsView(ListView):
+    model = Post
+
+
+class SinglePostView(DetailView):
     model = Post
