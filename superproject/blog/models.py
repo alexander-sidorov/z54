5ab07c6acd8e315d7672cf6a20a661923d05804d
Alexander Sidorov <alexander@sidorov.dev>
2021-09-28 21:33:58 +0300

    added migrations to connect Post >- User

--- superproject/blog/models.py
+++ superproject/blog/models.py
@@ -1,11 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse_lazy
+
+User = get_user_model()
 
 
 class Post(models.Model):
     title = models.TextField()
     content = models.TextField(blank=True, null=True)
     hidden = models.BooleanField(default=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def get_absolute_url(self) -> str:
         return reverse_lazy("blog:single", kwargs={"pk": self.pk})
